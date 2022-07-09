feaa920e8c20e524c17ad437fa703b2eec535f26
глеб зайцев <z.gl3b@yandex.ru>
2022-07-09 18:29:09 +0300

    added js suport

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'))
 const browser = require('browser-sync').create()
 
-
 function style() {
     return gulp.src('./src/*.scss')
         .pipe(sass())
@@ -19,8 +18,10 @@ function watch() {
 
     gulp.watch('./src/*.scss', style)
     gulp.watch('./public/*.html').on('change', browser.reload)
+    gulp.watch('./public/**/*.js').on('change', browser.reload)
 }
 
 exports.watch = watch;
 exports.style = style;
 exports.default = watch;
+
